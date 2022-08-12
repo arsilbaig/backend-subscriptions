f75f098ba18efe7c8a49ad0ada79318edec26d83
Arsalan Baig <arsilbbb@gmail.com>
2022-08-12 16:43:14 +0500

    minor changes

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -806,9 +806,9 @@ exports.isAccountExists = async (req, res) => {
         }
       }).then(function(userEmailInfo){
         if(userEmailInfo!= null){
-          res.status(200).send({ message: 'Entered '+ query + 'is already registered with another account', staus: false});
+          res.status(200).send({ message: 'Entered '+ query + ' is already registered with another account', staus: false});
         }else{
-          res.status(200).send({message: 'No '+ query + 'is associated to any account', status: true})
+          res.status(200).send({message: 'No '+ query + ' is associated to any account', status: true})
         }
       })
     }
